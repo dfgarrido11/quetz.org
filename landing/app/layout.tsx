3effbc55259a8fc1a798f65a1475a5a37a3c596a
Daniel Garrido <dgarrido11@gmail.com>
2026-03-31 04:30:01 +0200

    Revert "feat: deploy Rainforest Canopy landing with Quetzito and Quetzita mascots"
    This reverts commit 7cde01042e230b5180b223200b0db73a504078b1.

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -76,7 +76,6 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&family=Work+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="bg-quetz-cream">
         <Providers>{children}</Providers>
